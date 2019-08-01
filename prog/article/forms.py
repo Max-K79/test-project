2c3edf561e56e156742ef7e0035d18d83f1467e6
Max-K79 <52309235+Max-K79@users.noreply.github.com>
2019-08-01 10:33:02 +0900

    Add files via upload

---
+++ prog/article/forms.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from django.forms import ModelForm
+from article.models import Comments
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comments
+        fields = ['comments_text']
+
+
+#        exclude = ()
